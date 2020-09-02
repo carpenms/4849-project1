a6d8233cb4365d9542ec882a39519c2fea288fc5
carpenms <51542241+carpenms@users.noreply.github.com>
2020-09-02 15:22:56 -0600

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,19 +1,19 @@
 $(document).ready(function() { // do this when the document is loaded
-	$("#bdialer").show(); // show the element with ID "element"
-	$("#bcontacts, #badd").hide(); // hide the element with ID "otherElement"
+	$("#dialer").show(); // show the element with ID "element"
+	$("#contacts, #add").hide(); // hide the element with ID "otherElement"
 });
 
 $("bdialer").click(function() { // when "button_id" is clicked
-	$("bdialer").show(); // show element
-	$("bcontacts, badd").hide();	// hide other elements
+	$("#dialer").show(); // show element
+	$("#contacts, #add").hide();	// hide other elements
 });
 
 $("bcontacts").click(function() { // when "button_id" is clicked
-	$("bcontacts").show(); // show element
-	$("bdialer, badd").hide();	// hide other elements
+	$("#contacts").show(); // show element
+	$("#dialer, #add").hide();	// hide other elements
 });
 
 $("badd").click(function() { // when "button_id" is clicked
-	$("badd").show(); // show element
-	$("bcontacts, bdialer").hide();	// hide other elements
+	$("#add").show(); // show element
+	$("#contacts, #dialer").hide();	// hide other elements
 });
